3a348f5565428a92b4ec2c891092c3108c5d1519
agarnung <garnungalejandro@gmail.com>
2025-02-04 19:12:27 +0100

    TODO en S-B

--- 2025-XX-XX-split-bregman/main.cpp
+++ 2025-XX-XX-split-bregman/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     /**
+     * TODO hacer alguna prueba codificada. De momento va a ser solamente artículo teórico. 
      * Traducir split bregman a c++ y escribir teoría en variacional
      * @see https://github.com/ycynyu007/L1-norm-total-variation-inpainting-using-Split_Bregman-Iteration
      */
